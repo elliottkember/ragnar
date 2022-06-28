ac75b5fdc7f4085bb5c15e858db84eb0ff6c207e
Elliott Kember <elliott@openspace.ai>
2022-06-28 08:02:25 -0700

    Nearly

---
+++ legato/legatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  legatoApp.swift
+//  legato
+//
+//  Created by Elliott Kember on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct legatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
